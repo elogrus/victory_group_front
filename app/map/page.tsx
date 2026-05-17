9c8b4d15fdbba76c81b3c39c9b623ac3324fe01d
d1n8 <mr.dinyar@mail.ru>
2026-05-17 11:38:30 +0500

    fix map header

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -6,21 +6,13 @@ import { CreateTaskModal } from "@/features/Dashboard/ui/CreateTaskModal";
 import { useState } from "react";
 
 export default function MapPage() {
-    // Состояние модалки создания задачи для Хедера
-    const [isCreateModalOpen, setCreateModalOpen] = useState(false);
-
     return (
         <div className="flex flex-col h-screen overflow-hidden bg-background">
-            <Header onCreateClick={() => setCreateModalOpen(true)} />
+            <Header/>
 
             <main className="flex-1 overflow-hidden relative">
                 <ProjectMap />
             </main>
-
-            <CreateTaskModal
-                isOpen={isCreateModalOpen}
-                onClose={() => setCreateModalOpen(false)}
-            />
         </div>
     );
 }
